65c53ac349839d8e0b3d19f810d1241d631a5a29
Ruben Manuel <rubensapalo97@gmail.com>
2025-02-25 01:39:38 +0100

    Consulta simples com query methods

--- src/main/java/com/ruben/project/workshop/repository/PostRepository.java
+++ src/main/java/com/ruben/project/workshop/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.ruben.project.workshop.repository;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.ruben.project.workshop.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
