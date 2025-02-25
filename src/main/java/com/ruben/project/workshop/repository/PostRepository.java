a7078b102115724e61b0f6427aa42eea81380f9b
Ruben Manuel <rubensapalo97@gmail.com>
2025-02-25 11:52:08 +0100

    Consulta simples com @Query

--- src/main/java/com/ruben/project/workshop/repository/PostRepository.java
+++ src/main/java/com/ruben/project/workshop/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.ruben.project.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.ruben.project.workshop.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
--- src/main/java/com/ruben/project/workshop/util/URL.java
+++ src/main/java/com/ruben/project/workshop/util/URL.java
@@ -1,17 +1,14 @@
 package com.ruben.project.workshop.util;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class URL {
-	
-	public static String decodeParam(String text) {
-		try {
-			return URLDecoder.decode(text, "UFT-8");
-
-		} catch (UnsupportedEncodingException e) {
-			return "";
-		}
-	}
-
+    public static String decodeParam(String text) {
+        try {
+            return URLDecoder.decode(text, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
 }
